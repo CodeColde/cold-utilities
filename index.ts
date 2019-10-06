64d0c0fbec812684225a54e6c98b37494bb30877
codecolde <hayo.web@gmail.com>
2019-10-06 23:19:16 +0200

    Added indexOfAll as a utility function

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import readingTime, { isStringArray } from './src/reading-time';
 import pipe from './src/pipe';
 import stripHTMLTags from './src/strip-html-tags';
+import indexOfAll from './src/indexOfAll';
 
 export {
     readingTime,
     isStringArray,
     pipe,
-    stripHTMLTags
+    stripHTMLTags,
+    indexOfAll
 };
---
+++ tests/data/indexOfAll.data.ts
@@ -0,0 +1,12 @@
+export const numberArray = [1, 3, 2, 4, 3, 3, 1, 2, 5];
+export const stringArray = ['a', 'b', 'a', 'bc', 'a', 'cd', 'd', 'c', 'b'];
+export const mixedArray = ['a', 'b', 1, 2, 1, 'a', 2, 'b', 'a'];
+
+export const numberArrOne = [0, 6];
+export const numberArrThree = [1, 4, 5];
+
+export const stringArrA = [0, 2, 4];
+export const stringArrB = [1, 8];
+
+export const mixedArrA = [0, 5, 8];
+export const mixedArrOne = [2, 4];
